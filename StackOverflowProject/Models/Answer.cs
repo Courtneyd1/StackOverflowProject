3bb151b8de3ca39c64c00bad2f366c031f39b902
Courtney <courtneydavoli1@gmail.com>
2026-03-27 08:32:49 -0400

    added JsonPropertyName attributes, tightened up some cshtml logic.

--- StackOverflowProject/Models/Answer.cs
+++ StackOverflowProject/Models/Answer.cs
@@ -1,10 +1,19 @@
+using System.Text.Json.Serialization;
+
 namespace StackOverflowProject.Models
 {
     public class Answer
     {
-        public bool AcceptedAnswer { get; set; }
+        [JsonPropertyName("answer_id")]
         public int AnswerID { get; set; }
-        public string Body { get; set; } = string.Empty;
+
+        [JsonPropertyName("question_id")]
         public int QuestionID { get; set; }
+
+        [JsonPropertyName("body")]
+        public string Body { get; set; } = string.Empty;
+
+        [JsonPropertyName("score")]
+        public int Score { get; set; }
     }
 }
--- StackOverflowProject/Models/Question.cs
+++ StackOverflowProject/Models/Question.cs
@@ -1,10 +1,21 @@
+using System.Text.Json.Serialization;
+
 namespace StackOverflowProject.Models
 {
     public class Question
     {
-        public int AnswerCount { get; set; }
+        [JsonPropertyName("question_id")]
         public int QuestionID { get; set; }
+
+        [JsonPropertyName("title")]
         public string Title { get; set; } = string.Empty;
+
+        [JsonPropertyName("answer_count")]
+        public int AnswerCount { get; set; }
+
+        [JsonPropertyName("accepted_answer_id")]
+        public int? AcceptedAnswerID { get; set; }
+
         public List<Answer> Answers { get; set; } = new();
     }
 }
